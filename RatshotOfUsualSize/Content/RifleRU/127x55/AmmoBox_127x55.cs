08e2b4c98d8e381cb5de554aa2c3b14ca06025dd
bushtail <evannosich@icloud.com>
2025-12-16 12:56:40 -0500

    box templates

--- RatshotOfUsualSize/Content/RifleRU/127x55/AmmoBox_127x55.cs
+++ RatshotOfUsualSize/Content/RifleRU/127x55/AmmoBox_127x55.cs
@@ -7,7 +7,42 @@ namespace RatshotOfUsualSize;
 public record AmmoBox_127x55 : NewItemFromCloneDetails
 {
     public override MongoId? ItemTplToClone { get; set; } = ItemTpl.AMMOBOX_127X55_PS12_10RND;
-    public override TemplateItemProperties OverrideProperties { get; set; }
+    public override TemplateItemProperties OverrideProperties { get; set; } = new()
+    {
+        Prefab = new Prefab
+        {
+            Path = "pistol/ammobox_45acp_ratshot.bundle"
+        },
+        Name = "ammo_box_45acp_xx_rat",
+        StackMaxRandom = 10,
+        StackMaxSize = 1,
+        StackMinRandom = 10,
+        StackObjectsCount = 1,
+        StackSlots = 
+        [
+            new StackSlot
+            {
+                Id = "694069f818ee56f9490e1e61",
+                Name = "cartridges",
+                Parent = ModItemTpl.AMMOBOX_45ACP.ToString(),
+                MaxCount = 10,
+                Properties = new StackSlotProperties
+                {
+                    Filters = 
+                    [
+                        new SlotFilter
+                        {
+                            Filter = 
+                            [
+                                ModItemTpl.AMMO_45ACP
+                            ]
+                        }
+                    ]
+                },
+                Prototype = "5748538b2459770af276a261"
+            }
+        ]
+    };
     public override string ParentId { get; set; } = ParentID.AMMOBOX.ToString();
     public override string NewId { get; set; }
     public override double? FleaPriceRoubles { get; set; }
--- RatshotOfUsualSize/Content/RifleUS/762x51/AmmoBox_762x51.cs
+++ RatshotOfUsualSize/Content/RifleUS/762x51/AmmoBox_762x51.cs
@@ -7,7 +7,42 @@ namespace RatshotOfUsualSize;
 public record AmmoBox_762x51 : NewItemFromCloneDetails
 {
     public override MongoId? ItemTplToClone { get; set; } = ItemTpl.AMMOBOX_762X51_BCP_FMJ_20RND;
-    public override TemplateItemProperties OverrideProperties { get; set; } = new() { };
+    public override TemplateItemProperties OverrideProperties { get; set; } = new()
+    {
+        Prefab = new Prefab
+        {
+            Path = "pistol/ammobox_45acp_ratshot.bundle"
+        },
+        Name = "ammo_box_45acp_xx_rat",
+        StackMaxRandom = 10,
+        StackMaxSize = 1,
+        StackMinRandom = 10,
+        StackObjectsCount = 1,
+        StackSlots = 
+        [
+            new StackSlot
+            {
+                Id = "694069f818ee56f9490e1e61",
+                Name = "cartridges",
+                Parent = ModItemTpl.AMMOBOX_45ACP.ToString(),
+                MaxCount = 10,
+                Properties = new StackSlotProperties
+                {
+                    Filters = 
+                    [
+                        new SlotFilter
+                        {
+                            Filter = 
+                            [
+                                ModItemTpl.AMMO_45ACP
+                            ]
+                        }
+                    ]
+                },
+                Prototype = "5748538b2459770af276a261"
+            }
+        ]
+    };
     public override string ParentId { get; set; } = ParentID.AMMOBOX.ToString();
     public override string NewId { get; set; } = "";
     public override double? FleaPriceRoubles { get; set; }
